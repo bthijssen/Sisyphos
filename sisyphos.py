8a86d1d2badce35a45945b46088a0a22d0d097b5
bthijssen <b.f.k.thijssen@gmail.com>
2025-03-17 14:18:39 +0100

    Add push notification using Pushover

--- sisyphos.py
+++ sisyphos.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import beepy
+import requests
 from random import randint
 from dotenv import dotenv_values
 
@@ -17,6 +18,7 @@ class TicketChecker:
         self.driver = webdriver.Chrome(service=service)
         self.wait = WebDriverWait(self.driver, 10)
         self.url = config["URL"]
+        self.config = config
     
     def open_page(self):
         """Open the URL in the browser."""
@@ -29,15 +31,18 @@ class TicketChecker:
             try:
                 wait_time = randint(10,12)
                 buy_ticket_element = WebDriverWait(self.driver, wait_time).until(
-                    # EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Buy ticket')]"))
+                    # EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Reserved')]"))
                     EC.element_to_be_clickable(
                         # this only selects the tickets without KWbN membership needed
                         ((By.XPATH, "//button[contains(text(), 'Buy ticket')]"))
                     )
                 )
+                print("Buy ticket element detected")
                 buy_ticket_element.click()
+                tickets_available_text = "Tickets are available!"
+                self.send_push_notification(tickets_available_text)
                 beepy.beep() # Alert the user
-                print("Tickets are available!")
+                print(tickets_available_text)
                 break  # Exit loop after clicking the button
             except TimeoutException:
                 if self.driver.find_elements(By.XPATH, "//*[contains(text(), 'Too many attempts. Please wait a moment')]"):
@@ -55,6 +60,8 @@ class TicketChecker:
             self.check_tickets()
         except KeyboardInterrupt:
             print("Script stopped by user.")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
         finally:
             self.cleanup()
     
@@ -62,7 +69,18 @@ class TicketChecker:
         """Close the browser and clean up resources."""
         input("Press any key to exit...")  # Prevents the script from closing automatically
         self.driver.quit()
-
+        
+    def send_push_notification(self, message, title="Notification"):
+        url = self.config["PUSH_URL"]
+        data = {
+            "token": self.config["PUSH_API_TOKEN"],
+            "user": self.config["PUSH_USER_KEY"],
+            "message": message,
+            "title": title
+        }
+        response = requests.post(url, data=data)
+        print(response.json())  # Check the response
+    
 if __name__ == "__main__":
     config = dotenv_values(".env")
     options = Options() 
