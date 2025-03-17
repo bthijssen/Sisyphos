72073e188797a1ccb883fa9ecc43bf590cd91030
bthijssen <b.f.k.thijssen@gmail.com>
2025-03-17 16:53:27 +0100

    Set waittime to 13 and a print statements for logging purposes

--- sisyphos.py
+++ sisyphos.py
@@ -29,7 +29,21 @@ class TicketChecker:
         """Continuously check for ticket availability and click if available."""
         while True:
             try:
-                wait_time = randint(10,12)
+                wait_time = 13
+                
+                # Find all buttons with "Reserved" or "Buy ticket"
+                buttons = self.driver.find_elements(By.XPATH, "//button[contains(text(), 'Reserved') or contains(text(), 'Buy ticket')]")
+
+                # Check the text of each button
+                for index, button in enumerate(buttons):
+                    button_text = button.text.strip()
+                    print(f"Button {index + 1}: {button_text}")
+
+                    if "Reserved" in button_text:
+                        print(f"Button {index + 1} shows 'Reserved'.")
+                    elif "Buy ticket" in button_text:
+                        print(f"Button {index + 1} shows 'Buy ticket'.")
+                    
                 buy_ticket_element = WebDriverWait(self.driver, wait_time).until(
                     # EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Reserved')]"))
                     EC.element_to_be_clickable(
@@ -37,12 +51,15 @@ class TicketChecker:
                         ((By.XPATH, "//button[contains(text(), 'Buy ticket')]"))
                     )
                 )
-                print("Buy ticket element detected")
+                
+                print("Buy ticket element click")
                 buy_ticket_element.click()
+                
                 tickets_available_text = "Tickets are available!"
                 self.send_push_notification(tickets_available_text)
                 beepy.beep() # Alert the user
                 print(tickets_available_text)
+                
                 break  # Exit loop after clicking the button
             except TimeoutException:
                 if self.driver.find_elements(By.XPATH, "//*[contains(text(), 'Too many attempts. Please wait a moment')]"):
