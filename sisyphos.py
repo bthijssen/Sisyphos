45b81a1b1509e0a16ac48d4819fdebb38a45d330
bthijssen <b.f.k.thijssen@gmail.com>
2025-03-17 13:15:04 +0100

    Adjusted to work on the new website

--- sisyphos.py
+++ sisyphos.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import beepy
+from random import randint
 from dotenv import dotenv_values
 
 class TicketChecker:
@@ -20,26 +22,28 @@ class TicketChecker:
         """Open the URL in the browser."""
         self.driver.get(self.url)
         self.wait.until(EC.presence_of_element_located((By.XPATH, "//body")))
-        # click through the cooking window
-        cookie_continue_element = self.wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'OK, continue')]"))
-        )
-        cookie_continue_element.click()
     
     def check_tickets(self):
         """Continuously check for ticket availability and click if available."""
         while True:
             try:
-                ticket_kopen_element = self.wait.until(
+                wait_time = # randint(10,12)
+                buy_ticket_element = WebDriverWait(self.driver, wait_time).until(
+                    # EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Buy ticket')]"))
                     EC.element_to_be_clickable(
-                        (By.XPATH, "//*[self::a or self::button][contains(., 'Ticket kopen') or contains(., 'Buy ticket')]")
+                        # this only selects the tickets without KWbN membership needed
+                        ((By.XPATH, "//button[contains(text(), 'Buy ticket')]"))
                     )
                 )
+                buy_ticket_element.click()
+                beepy.beep() # Alert the user
                 print("Tickets are available!")
-                ticket_kopen_element.click()
-                beepy.beep()  # Alert the user
                 break  # Exit loop after clicking the button
             except TimeoutException:
+                if self.driver.find_elements(By.XPATH, "//*[contains(text(), 'Too many attempts. Please wait a moment')]"):
+                    sleep_time = 30
+                    print(f"Sleeping {sleep_time} seconds")
+                    time.sleep(sleep_time)
                 print("Tickets are not available at the moment. Checking again...")
                 self.driver.refresh()
                 self.wait.until(EC.presence_of_element_located((By.XPATH, "//body")))
